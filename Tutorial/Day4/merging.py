eb831601cd8785ec2effabcab09842bc6d263fef
alekhaya99 <alekhaya.datta@gmail.com>
2020-09-12 21:44:40 +0800

    Day4 updated

--- Tutorial/Day4/merging.py
+++ Tutorial/Day4/merging.py
@@ -1,2 +1,10 @@
 import  cv2
 import numpy as np
+
+Image=cv2.imread('../../Assets/Card.jpg')
+Horizantal=np.hstack((Image,Image))#For Horizantal Stacking
+Vertical=np.vstack((Image,Image))#For Vertical Stacking
+
+cv2.imshow("Horizantal",Horizantal)
+cv2.imshow("Diagonal",Vertical)
+cv2.waitKey(0)
