b04ea99274d8a9a98033a5d395aba45484ba7e89
alekhaya99 <alekhaya.datta@gmail.com>
2020-09-15 22:38:49 +0800

    Day5

---
+++ Tutorial/Day5/Shape.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+#Finding the Countours
+def getContours(Image):
+    contours,hierarchy = cv2.findContours(Image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        #Prints the approx area of the shapes
+        print(area)
+        if area>500:
+            #-1 because all the contours will be drawn
+            cv2.drawContours(imgContour, cnt, -1, (0,255, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor ==3: objectType ="Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
+                else:objectType="Rectangle"
+            elif objCor>4: objectType= "Circles"
+            else:objectType="None"
+
+
+
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContour,objectType,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (0,0,0),2)
+
+
+Folder='../../Assets/Shapes.png'
+Image=cv2.imread(Folder)
+imgContour=Image.copy()
+
+Image_Gray=cv2.cvtColor(Image,cv2.COLOR_BGR2GRAY)
+'''
+The second parameter for the Gaussian Blur is the Kernel can be found here: https://docs.opencv.org/master/d4/dbd/tutorial_filter_2d.html
+The  next one is the sigma, If we want to have more blur we need to make the sigma value higher
+'''
+Image_Blur=cv2.GaussianBlur(Image,(5,5),10)
+#The below syntax detects the edges of the Image, canny image requires a threshold value
+Image_Canny=cv2.Canny(Image,100,100)
+getContours(Image_Canny)
+
+Stack_Image=stackImages(0.5,([Image_Blur,Image_Gray,Image],[Image_Canny,imgContour,Image]))
+
+cv2.imshow("Output Image",Stack_Image)
+cv2.waitKey(0)
